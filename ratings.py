04782735a8419dbd7775e34f0b3f77a3d484da21
vyvan56@hotmail.com <vyvanhuynh@Vys-MacBook-Pro.local>
2021-10-04 14:49:48 -0500

    finished getting user input choice

--- ratings.py
+++ ratings.py
@@ -5,24 +5,34 @@ import sys
 def restaurant_rating_dict(file):
     text = open(sys.argv[1])
     scores_dict = {}
+    quit = True
 
-    restaurant_input = input("What restaurant would you like to add?: ")
-    rating_input = input("What would you rate the place (0-5): ")
+    while quit == True:
+        restaurant_input = input("What restaurant would you like to add?: ")
+        rating_input = input("What would you rate the place (0-5): ")
+        if int(rating_input) > 5:
+            print("Please enter a number between 0 and 5")
+        else:
+            scores_dict[restaurant_input] = rating_input #creates a dictionary with line 9 and 10
+            for line in text:
+                restaurant_strip = line.rstrip()
+                restaurant = restaurant_strip.split(":")
+                place = restaurant[0]
+                rating = restaurant[1]
+                scores_dict[place] = rating
 
-    scores_dict[restaurant_input] = rating_input #creates a dictionary with line 9 and 10
-
-    # for place_input,rating_input in sorted(scores_dict.items()):
-    #     print(f"{place_input} is rated at {rating_input}.")
-
-    for line in text:
-        restaurant_strip = line.rstrip()
-        restaurant = restaurant_strip.split(":")
-        place = restaurant[0]
-        rating = restaurant[1]
-        scores_dict[place] = rating
-
-    for place,rating in sorted(scores_dict.items()):
-        print(f"{place} is rated at {rating}.")
+            for place,rating in sorted(scores_dict.items()):
+                print(f"{place} is rated at {rating}.")
+            
+            print("Did you want to add more restaurants?")
+            print("1. Add more restaurants")
+            print("2. Do not add anymore restaurants")
+            user_choice = int(input("Please enter 1. to add more retaurants or 2. to stop adding more restaurants: "))
+            if user_choice == 1:
+                continue
+            if user_choice == 2:
+                print("Ok, thank you for your restaurants and ratings")
+                break
     
 
 restaurant_rating_dict("scores.txt")
